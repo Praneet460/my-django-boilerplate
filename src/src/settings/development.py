cccb4c131e6706d8926d1f5fd6435da2bad8b255
Praneet Nigam <pnigam@pokerbaazi.in>
2020-12-21 02:23:06 +0530

    add allauth

---
+++ src/src/settings/development.py
@@ -0,0 +1,10 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+
+INSTALLED_APPS += []
+
+MIDDLEWARE += []
